c802f0d1962966e86b0fb2baa9ed726a2b9dbfcd
BinarySahil <162962503+BinarySahil@users.noreply.github.com>
2024-03-11 14:12:41 +0530

    Add files via upload

---
+++ Schema.sql
@@ -0,0 +1,87 @@
+CREATE TABLE product_category (
+
+  id INT PRIMARY KEY AUTO_INCREMENT,
+
+  name VARCHAR(255) NOT NULL,
+
+  description TEXT,
+
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  deleted_at TIMESTAMP DEFAULT NULL
+
+);
+
+
+
+CREATE TABLE product (
+
+  id INT PRIMARY KEY AUTO_INCREMENT,
+
+  name VARCHAR(255) NOT NULL,
+
+  description TEXT,
+
+  SKU VARCHAR(255) NOT NULL,
+
+  category_id INT NOT NULL,
+
+  inventory_id INT,
+
+  price DECIMAL(10,2) NOT NULL,
+
+  discount_id INT,
+
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  deleted_at TIMESTAMP DEFAULT NULL,
+
+  FOREIGN KEY (category_id) REFERENCES product_category(id),
+
+  FOREIGN KEY (inventory_id) REFERENCES product_inventory(id),
+
+  FOREIGN KEY (discount_id) REFERENCES discount(id)
+
+);
+
+
+
+CREATE TABLE product_inventory (
+
+  id INT PRIMARY KEY AUTO_INCREMENT,
+
+  quantity INT NOT NULL,
+
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  deleted_at TIMESTAMP DEFAULT NULL
+
+);
+
+
+
+CREATE TABLE discount (
+
+  id INT PRIMARY KEY AUTO_INCREMENT,
+
+  name VARCHAR(255) NOT NULL,
+
+  description TEXT,
+
+  discount_percent DECIMAL(5,2) NOT NULL,
+
+  active BOOLEAN NOT NULL,
+
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  deleted_at TIMESTAMP DEFAULT NULL
+
+);
